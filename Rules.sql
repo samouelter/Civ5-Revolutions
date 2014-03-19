e6646e73cbc75b6d3cf4b671a72c6105002b2203
Gedemon <gedemon@free.fr>
2014-03-19 03:46:23 +0100

    DB Initialization compatibility fix

--- Rules.sql
+++ Rules.sql
@@ -5,12 +5,12 @@ They are updated at game start and reload by RevolutionDefines.lua
 
 -- Relation thresholds (max value, ex: 95 = Joyful, -60 = Unhappy)
 
-INSERT INTO Defines (Name, Value) VALUES ('THRESHOLD_JOYFUL', 200);
-INSERT INTO Defines (Name, Value) VALUES ('THRESHOLD_HAPPY', 75);
-INSERT INTO Defines (Name, Value) VALUES ('THRESHOLD_CONTENT', 10);
-INSERT INTO Defines (Name, Value) VALUES ('THRESHOLD_UNHAPPY', -10);
-INSERT INTO Defines (Name, Value) VALUES ('THRESHOLD_WOEFUL', -125);
-INSERT INTO Defines (Name, Value) VALUES ('THRESHOLD_EXASPERATED', -300);
+INSERT OR REPLACE INTO Defines (Name, Value) VALUES ('THRESHOLD_JOYFUL', 200);
+INSERT OR REPLACE INTO Defines (Name, Value) VALUES ('THRESHOLD_HAPPY', 75);
+INSERT OR REPLACE INTO Defines (Name, Value) VALUES ('THRESHOLD_CONTENT', 10);
+INSERT OR REPLACE INTO Defines (Name, Value) VALUES ('THRESHOLD_UNHAPPY', -10);
+INSERT OR REPLACE INTO Defines (Name, Value) VALUES ('THRESHOLD_WOEFUL', -125);
+INSERT OR REPLACE INTO Defines (Name, Value) VALUES ('THRESHOLD_EXASPERATED', -300);
 
 
 -- remove Rebels from vanilla game
